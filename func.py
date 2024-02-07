f1ba154295150b2f33e781d155b853c2075b42b0
Dominik <dominikpalka598@gmail.com>
2024-02-07 15:21:41 +0100

    Some changes

--- func.py
+++ func.py
@@ -1,4 +1,6 @@
-def Byte_Conv(x): # only for ints ;C
+import os
+
+def Byte_Conversion(x): # only for ints ;C
     check1 = True
     byte_size = 1
     while check1:
@@ -8,3 +10,39 @@ def Byte_Conv(x): # only for ints ;C
         except OverflowError:
             byte_size += 1
     return x
+
+def Progress_Bar(progress, total):
+    percent = 100 * (progress / total)
+    bar = chr(9608) * percent + chr(9617) * (100 - percent)
+    print(f"|{bar} {percent:.2f}%|", end="\r")
+
+def Image_To_Dict(w, h, pix): # converts image to dictionary of colored pixels
+    data = {}
+    pos = 0
+    for i in range(w):
+        for y in range(h):
+            if '#%02x%02x%02x' % pix[i, y] not in data:
+                data['#%02x%02x%02x' % pix[i, y]] = []
+            data['#%02x%02x%02x' % pix[i, y]].append(pos)
+            pos += 1
+    print("\nPixel data added to dictionary")
+    return data
+
+def File_Exist(file_name):
+    if not os.path.isfile(file_name): # create output file if it doesnt exist que????????????????
+        file = open(file_name, "x")
+        file.close()
+        print("Output file '" + file_name + "' created")
+        return False
+    print("Output file detected")
+    return True # returns true or false, it depends if file existed or not, maybe usefull later, just maybe, idk but I will leave it here 
+
+def Encode_Chars(string):
+    return string.encode('utf-8')
+
+def Write_Image_Info(file_name, w, h, data):
+    file = open(file_name, "wb")
+    file.write(Encode_Chars(str(w) + 'x' + str(h) + '\n'))
+    file.write(Encode_Chars(str(data).replace(" ", "")))
+    file.close()
+    print("Info added succesfully")
--- main.py
+++ main.py
@@ -3,47 +3,14 @@
 # jeśli jakiś kolor będzie najwięcej razy używany będzie ustawiony na default dla pozostałych pikseli
 # to samo będzie sie działo z kanałem alpha dla plików png
 # photo with avant-garde compression - pwac, it sounds fancy -> picture with state_of_the_art compression, yes state of the art is one word here dont judge me
-import os
 from PIL import Image
-from func import Byte_Conv
-img = Image.open('serdek.png')
+from func import Image_To_Dict, File_Exist, Write_Image_Info
+
+ifn = "serdek.png" # input file name
+ofn = "data.pwsc" # output file name
+img = Image.open(ifn)
 pix = img.load()
 w ,h = img.size
-data = {}
-pos = 0
-for i in range(w):
-    for y in range(h):
-        if '#%02x%02x%02x' % pix[i, y] not in data:
-            data['#%02x%02x%02x' % pix[i, y]] = []
-        data['#%02x%02x%02x' % pix[i, y]].append(pos)
-        pos += 1
-print("\nPixel data added to dictionary")
-
-ofn = "data.pwsc" # output file name
-if not os.path.isfile(ofn): # create output file if it doesnt exist
-    file = open(ofn, "x")
-    file.close()
-    print("Output file '" + ofn + "' created")
-
-file = open(ofn, "wb")
-if w == h:
-    file.write(str(w).encode('utf-8'))
-else:
-    file.write(Byte_Conv((str(w) + "x" + str(h)).encode('utf-8')))
-max_val = max(data, key=data.get)
-file.write(('\n' + max_val).encode('utf-8'))
-file.close()
-print("Image stored succesfully\n")
-
-with open(ofn, 'rb') as file:
-    binary_data = file.read()
-    print(binary_data.decode('utf-8'))
-
-
-# TODO 
-# 1 dodaj os sprawdzanie czy plik data.pwac istnieje
-# 2 potrzeba procedularnego generowania pliku, początek pliku to główne rzeczy typu wysokośc 512x512, główny kolor do zamiany, ustalenie znaków które mało ważą itp
-# 3 nie wiem kurwa napij sie czegoś bo tu jebniesz
-
-
-# updaż!!!! zadeklaruj kolory jako np. a,b,c ale te najbardziej common i potem będzie np a+9, b-14 jak na dużej palecdie kolorów
+data = Image_To_Dict(w, h, pix)
+File_Exist(ofn)
+Write_Image_Info(ofn, w, h, data)
